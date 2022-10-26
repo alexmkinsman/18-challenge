e7d5beee2dc93d795f2a9251745cb48500e71aa8
Alexandra Kinsman <alexandrakinsman@Alexs-Mac.lan>
2022-10-25 17:18:26 -0700

    config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./config/connection.js');
 
 const PORT = process.env.PORT || 3001
 const app = express();
@@ -7,8 +8,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-//db.once("open", () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on ${PORT}!`)
-  })
-//})
+  });
+});
